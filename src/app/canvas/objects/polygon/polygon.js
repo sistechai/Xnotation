70b4aef624e54c44d6706cdc296ff79411281bc5
Zvyozdo4ka <yulduzprogramist@yandex.ru>
2021-12-30 17:22:11 +0900

    previous code did not work properly.

--- src/app/canvas/objects/polygon/polygon.js
+++ src/app/canvas/objects/polygon/polygon.js
@@ -226,15 +226,15 @@ function generatePolygon() {
 
     const lengthArray = pointsNewLine.length;
     console.log("+++ lengthArray", lengthArray)
-    //let i;
+    let i;
     let tempArrayLine = [];
 
       tempArrayLine.push(...pointsNewLine);
 
-      //for (i = lengthArray- 1; i>-1; i--) {
+    for (i = lengthArray- 1; i>-1; i--) {
         //console.log("i ", i);
-        tempArrayLine.push(pointsNewLine[lengthArray- 1])
-      //}
+        tempArrayLine.push(pointsNewLine[i]);
+    }
     console.log("+++ tempArrayLine", tempArrayLine);
 
       polygon = new fabric.Polygon(tempArrayLine, polygonProperties.newPolygon()); // for now, got it from if cycle above

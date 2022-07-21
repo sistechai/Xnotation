35029022b43d29e766a1a2420d2d6cf5d52eddd2
Shekhrozx <shekhrozx@gmail.com>
2022-07-21 14:52:26 +0900

    module import rearranged

--- src/app/index.js
+++ src/app/index.js
@@ -1,23 +1,34 @@
-import { registerHotKeys } from './keyEvents/keyboard/hotKeys.js';
-import registerWindowMouseEvents from './keyEvents/mouse/registerEvents.js';
+/**
+ * @author Bryan & Julianna
+ * @version 0.1
+ * @description This is online Image annotation tool.
+ * 
+ */
+
 import { findUserOS } from './tools/OS/OSManager.js';
+import { applyStyling } from './tools/globalStyling/style.js';
+
 import { constructCanvas } from './canvas/canvas.js';
+import { registerHotKeys } from './keyEvents/keyboard/hotKeys.js';
+
 import initialiseToolkit from './tools/toolkit/init.js';
 import initialiseLabellerModal from './tools/labellerModal/buttons.js';
 import { initialiseSettingsPopup } from './tools/settingsPopup/init.js';
+import registerWindowMouseEvents from './keyEvents/mouse/registerEvents.js';
+import { initialiseWindowDimService } from './tools/dimWindow/dimWindowService.js';
+import { initialiseCoreButtonPopovers } from './tools/globalStyling/buttons/popovers.js';
 import assignPassiveEventListeners from './tools/passiveEventListeners/passiveEventListeners.js';
-import initialiseShapeManipulationDeltas from './canvas/objects/deltaValueSetters/initialiseShapeManipulationDeltas.js';
-import initialiseDragAndDropFunctionality from './tools/dragAndDrop/dragAndDrop.js';
+
+import initialiseRemoveImagesModal from './tools/imageList/removeImages/modal/init.js';
 import initialiseImageListFunctionality from './tools/imageList/init.js';
 import initialiseLabelListFunctionality from './tools/labelList/init.js';
-import initialiseRemoveImagesModal from './tools/imageList/removeImages/modal/init.js';
-import { initialiseCoreButtonPopovers } from './tools/globalStyling/buttons/popovers.js';
-import { applyStyling } from './tools/globalStyling/style.js';
+import initialiseDragAndDropFunctionality from './tools/dragAndDrop/dragAndDrop.js';
 import { initialiseImageSwitchPanelFunctionality } from './tools/imageSwitchPanel/style.js';
 import { initialisePulseAnimationCancelling } from './tools/utils/buttons/pulseAnimation.js';
-import { initialiseWindowDimService } from './tools/dimWindow/dimWindowService.js';
+import initialiseShapeManipulationDeltas from './canvas/objects/deltaValueSetters/initialiseShapeManipulationDeltas.js';
 import initialiseBrowserExitHandler from './tools/browserExit/browserExitHandler.js';
 
+// ------------------------------------------------------------
 // Debugging Mode
 window.DEBUG = true;
 
@@ -26,8 +37,8 @@ applyStyling();
 
 // Initialized only once, when the page id downloaded, before first image downloaded
 constructCanvas();
-
 registerHotKeys();
+
 initialiseToolkit();
 initialiseLabellerModal();
 initialiseSettingsPopup();
@@ -35,6 +46,7 @@ registerWindowMouseEvents();
 initialiseWindowDimService();
 initialiseCoreButtonPopovers();
 assignPassiveEventListeners();
+
 initialiseRemoveImagesModal();
 initialiseImageListFunctionality();
 initialiseLabelListFunctionality();
